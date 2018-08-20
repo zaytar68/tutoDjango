5f29e7c49b9b42a9d9bb2a7a5fb1bae6d42712e2
zaytar <cedric.tirolf@gmail.com>
2018-08-20 21:24:03 +0200

    P102 OK

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'$', views.post_list, name='post-list'),
+    #url(r'$', views.post_list, name='post-list'),
+    path('', views.post_list, name='post-list'),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
