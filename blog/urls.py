6225b9b07e147edd37f0e38aac2c8b353bfebd3c
zaytar <cedric.tirolf@gmail.com>
2018-08-21 20:35:32 +0200

    Ajout de vues qui permettent de créer et d'éditer un post

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.post_list, name='post-list'),
     #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-]
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/new/', views.post_new, name='post_new'),
+    ]
